63a7078ab390823058664a3dbe2b11d0dc7aeadd
Ardalan Maroof <000490109@student.vcc.ca>
2025-03-24 11:57:05 -0700

    Firebase Config added to React

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBUy1mSaXrIiGwV8KK3oJaE91295g4o-dE",
+  authDomain: "travel-itinerary-app-338a6.firebaseapp.com",
+  projectId: "travel-itinerary-app-338a6",
+  storageBucket: "travel-itinerary-app-338a6.firebasestorage.app",
+  messagingSenderId: "350802060239",
+  appId: "1:350802060239:web:67f9cc9e3bc269758ea8ba"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
